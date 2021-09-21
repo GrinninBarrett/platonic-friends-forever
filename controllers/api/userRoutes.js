c892c0f179e0957a9bc37d6813357c5f061321bc
Tucker Barrett <tuckerbarrett@gmail.com>
2021-09-21 16:33:25 -0400

    Fixes login form, create account form, and logout button

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -38,10 +38,6 @@ router.post('/', async (req, res) => {
   try {
     const userData = await User.create(req.body);
 
-    res.status(200).json(userData);
-
-    console.log(userData);
-
     req.session.save(() => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
@@ -117,8 +113,6 @@ router.post('/login', async (req, res) => {
       
       res.json({ user: userData, message: 'You are now logged in!' });
     });
-
-   
 
   } catch (err) {
     res.status(400).json(err);
