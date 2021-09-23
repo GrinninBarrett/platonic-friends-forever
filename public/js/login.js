e85f7b0abd0666f3cba6e3c2c8c6f6379e4bffea
Tucker Barrett <tuckerbarrett@gmail.com>
2021-09-23 15:27:48 -0400

    Adds styling to signup page, makes fields required

--- public/js/login.js
+++ public/js/login.js
@@ -25,13 +25,13 @@ const loginFormHandler = async (event) => {
 const signupFormHandler = async (event) => {
   event.preventDefault();
 
-  const name = document.querySelector("#name-signup").value.trim();
-  const first_name = name.split(' ')[0];
-  const last_name = name.split(' ')[1];
+  const first_name = document.querySelector("#first-name").value.trim();
+  const last_name = document.querySelector("#last-name").value.trim();
   const email = document.querySelector("#email-signup").value.trim();
   const password = document.querySelector("#password-signup").value.trim();
 
-  if (name && email && password) {
+  if (first_name && last_name && email && password) {
+    console.log(first_name)
     const response = await fetch("/api/users", {
       method: "POST",
       body: JSON.stringify({ first_name, last_name, email, password }),
