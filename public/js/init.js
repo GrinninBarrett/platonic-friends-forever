d9825d0966851e0e5a6bc4c6baa0dac4552c90f8
Tucker Barrett <tuckerbarrett@gmail.com>
2021-09-23 13:42:46 -0400

    Changes nav layout in main.handlebars

--- public/js/init.js
+++ public/js/init.js
@@ -1,7 +1,3 @@
-(function($){
-  $(function(){
-
-    $('.sidenav').sidenav();
-
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+document.addEventListener('DOMContentLoaded', function() {
+  M.AutoInit();
+});
