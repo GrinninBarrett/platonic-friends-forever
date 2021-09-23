4da7acda42e795341eb842457406abef834b3bdc
copernichris <christhomas2101@gmail.com>
2021-09-23 12:14:14 -0400

    friends page added and links working

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const tagRoutes = require('./tagRoutes');
 const userTagRoutes = require('./userTagRoutes');
+const userLikeRoutes = require('./userLikeRoutes');
 
 router.use('/users', userRoutes);
 router.use('/tags', tagRoutes);
 router.use('/usertags', userTagRoutes);
+router.use('/userlikes', userLikeRoutes);
 
 module.exports = router;
