f061c9c6809c2f7835a29cde5b66f6f6ca43f198
Tucker Barrett <tuckerbarrett@gmail.com>
2021-09-24 16:15:32 -0400

    Starts edit user function in script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,17 @@
 const deleteButton = document.querySelector("#confirm-user-delete");
+
 const followButtons = document.querySelectorAll(".follow-button");
 const dismissButtons = document.querySelectorAll(".dismiss-button");
 
+// Declare elements from the form to edit user information
+const updateFirstNameEl = document.querySelector("#edit-first-name");
+const updateLastNameEl = document.querySelector("#edit-last-name");
+const updateCityEl = document.querySelector("#edit-city");
+const updateStateEl = document.querySelector("#edit-state");
+const updateBioEl = document.querySelector("#edit-bio");
+const updatePasswordEl = document.querySelector("#edit-password");
+
+// Toast to show when following a user
 const followToast = function() {
   M.toast({
     html: 'Followed user',
@@ -10,6 +20,7 @@ const followToast = function() {
   })
 }
 
+// Toast to show when dismissing a user
 const dismissToast = function() {
   M.toast({
     html: 'Dismissed user',
@@ -18,6 +29,7 @@ const dismissToast = function() {
   })
 }
 
+// Add event listeners to all buttons to initiate toasts
 for (let i = 0; i < followButtons.length; i++) {
   followButtons[i].addEventListener("click", followToast);
 }
@@ -26,6 +38,8 @@ for (let i = 0; i < dismissButtons.length; i++) {
   dismissButtons[i].addEventListener("click", dismissToast);
 }
 
+
+// After a user is deleted, log them out as well, returning to the homepage
 const logOutDeletedUser = async () => {
   const response = await fetch('/api/users/logout', {
     method: "POST",
@@ -42,7 +56,7 @@ const logOutDeletedUser = async () => {
 
 }
 
-
+// Fetch request to delete a user
 const handleDeleteUser = (event) => {
   event.preventDefault();
 
@@ -55,5 +69,50 @@ const handleDeleteUser = (event) => {
 
 }
 
-
+// Event listener to delete user
 deleteButton.addEventListener("click", handleDeleteUser);
+
+
+
+// Allow a user to edit their profile information
+const handleUpdateUser = (event) => {
+  event.preventDefault();
+
+  if (!(updateFirstNameEl.value.trim()) 
+      || (updateLastNameEl.value.trim()) 
+      || (updateCityEl.value.trim())
+      || (updateStateEl.value.trim())
+      || (updateBioEl.value.trim())
+      || (updatePasswordEl.value.trim())
+      ) {
+        return;
+      }
+
+  // For each element, check if it has text, indicating the user wants to edit that field
+  if (updateFirstNameEl.value.trim().length) {
+    const first_name = updateFirstNameEl.value.trim();
+  }
+
+  if (updateLastNameEl.value.trim().length) {
+    const last_name = updateLastNameEl.value.trim();
+  }
+
+  if (updateCityEl.value.trim().length) {
+    const city = updateCityEl.value.trim();
+  }
+
+  if (updateStateEl.value.trim().length) {
+    const state = updateStateEl.value.trim();
+  }
+
+  if (updateBioEl.value.trim().length) {
+    const bio = updateBioEl.value.trim();
+  }
+
+  if (updatePasswordEl.value.trim().length) {
+    const password = updatePasswordEl.value.trim();
+  }
+
+  
+
+}
